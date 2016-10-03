f23a99dbc9aa004049cb1f976fdffbf97eea171b
Alexander Chizhov <adchizhov@mail.ru>
2016-10-03 12:23:45 +0300

    first flask with template

--- first_flask.py
+++ first_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,7 +7,15 @@ def index():
 
 @app.route('/hello')
 def hello():
-	return 'Hello, my name is Adolf'
+	return 'Welcome to my ultimate first amazing godlike server!!! Let the moneyrain begins!'
+
+@app.route('/hello/<username>')
+def user(username):
+	return render_template('color_user.html', name=username)
+
+@app.route('/hello/day/<int:day>')
+def today(day):
+	return 'Maybe today is {} day'.format(day)
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
