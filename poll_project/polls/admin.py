0c13a73500c2db9d909b2afe9283f593bc36755b
Alexander Chizhov <adchizhov@mail.ru>
2016-11-11 13:48:19 +0300

    completed django tutorial for beginners

--- poll_project/polls/admin.py
+++ poll_project/polls/admin.py
@@ -1,5 +1,47 @@
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
 
-admin.site.register(Question)
+# 1 вариант
+# admin.site.register(Question) меняется все как обычно
+
+# 2 вариант
+# class QuestionAdmin(admin.ModelAdmin):
+# 	fields = ['pub_date', 'question_text'] # меняем поля местами в админке
+# admin.site.register(Question, QuestionAdmin) # и собственно регистриуем наши хотелки
+
+# 3 вариант регистрации поля Question
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None,               {'fields': ['question_text']}), # еще более круто
+#         ('Date information', {'fields': ['pub_date']}), # еще более круто
+#     ]
+# admin.site.register(Question, QuestionAdmin) # регистрируем
+
+# Простой вариант региcтрации поля Choice
+# admin.site.register(Choice)
+
+# Более крутой вариант 4 и с вопросами и с выбором
+# Здесь  Choice Question
+
+class ChoiceInline(admin.TabularInline): # а можно в стопку (admin.StackedInline)
+	model = Choice
+	extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}), # еще более круто
+        ('Date information', {'fields': ['pub_date']}), # еще более круто
+    ]
+    inlines = [ChoiceInline] # класс с выбором: Это говорит джанго
+    # Choice объекты редактируются на странице Question. По умолчанию
+    # обеспечь нам 3 выбора!
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently') # это здесь потому что изначально 
+    # все выводит в стринговом формате каждый объект, но приятнее по индивидуальному полю
+    # для этого используется переменная list_display с тьюплом 
+
+    list_filter = ['pub_date'] # добавили фильтр по полю даты публикации см. админку
+    search_fields = ['question_text'] # и даже теперь можно икать по вопросу!
+
+admin.site.register(Question, QuestionAdmin) # регистрируем!
--- poll_project/polls/views.py
+++ poll_project/polls/views.py
@@ -10,65 +10,65 @@ from django.utils import timezone
 
 # вариант с БОЛЬШИМ количеством кода для понимания
 # def index(request):
-# 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-# 	# template = loader.get_template('polls/index.html') not good way
-# 	context = {'latest_question_list': latest_question_list,}
-# 	# return HttpResponse(template.render(context, request)) not good way
-# 	return render(request, 'polls/index.html', context)
-	
+#   latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#   # template = loader.get_template('polls/index.html') not good way
+#   context = {'latest_question_list': latest_question_list,}
+#   # return HttpResponse(template.render(context, request)) not good way
+#   return render(request, 'polls/index.html', context)
+    
 # def detail(request, question_id):
-# 	# not fast way!
-# 	# try:
-# 		# question = Question.objects.get(pk=question_id)
-# 	# except Question.DoesNotExist:
-# 		# raise Http404('Question does not exist')
+#   # not fast way!
+#   # try:
+#       # question = Question.objects.get(pk=question_id)
+#   # except Question.DoesNotExist:
+#       # raise Http404('Question does not exist')
 
-# 	question = get_object_or_404(Question, pk=question_id) # get_list_or_404 (не гет, а фильтр если хочешь!)
-# 	return render(request, 'polls/detail.html', {'question': question})
+#   question = get_object_or_404(Question, pk=question_id) # get_list_or_404 (не гет, а фильтр если хочешь!)
+#   return render(request, 'polls/detail.html', {'question': question})
 
-# 	return HttpResponse('You are looking at question {}.'.format(question_id))
+#   return HttpResponse('You are looking at question {}.'.format(question_id))
 
 # def results(request, question_id):
-# 	question = get_object_or_404(Question, pk=question_id)
-# 	context = {'question': question} # можно просто в ретерн рендер в конце забахать
-# 	return render(request, 'polls/results.html', context)
+#   question = get_object_or_404(Question, pk=question_id)
+#   context = {'question': question} # можно просто в ретерн рендер в конце забахать
+#   return render(request, 'polls/results.html', context)
 
 # вариант с МЕНЬШИМ количеством кода лучше
 
 class IndexView(generic.ListView):
-	template_name = 'polls/index.html'
-	context_object_name = 'latest_question_list'
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-	def get_queryset(self): 
-		"""Вернет последние 5 вопросов c нормальным условием по времени!"""
-		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+    def get_queryset(self): 
+        """Вернет последние 5 вопросов c нормальным условием по времени!"""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
-	model = Question
-	template_name = 'polls/detail.html'
+    model = Question
+    template_name = 'polls/detail.html'
 
-	def get_queryset(self):
-		"""
+    def get_queryset(self):
+        """
         Исключает вопросы которые еще не опубликованы.
         """
         return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
-	model = Question
-	template_name = 'polls/results.html'
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	try:
-		selected_choice = question.choice_set.get(pk=request.POST['choice'])
-	except (KeyError, Choice.DoesNotExist):
-		return render(request, 'polls/detail.html', 
-			{'question': question, 'error_message': 'You did not select a choice.',})
-	else:
-		selected_choice.votes += 1
-		selected_choice.save()
-		# Всегда возврщай HttpResponseRedirect после того как получил
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', 
+            {'question': question, 'error_message': 'You did not select a choice.',})
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Всегда возврщай HttpResponseRedirect после того как получил
         # POST данные. Это поможет избежать случаев повторного (неверного) получения данных
         # если пользователь нажмет "назад".
 
-	return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
